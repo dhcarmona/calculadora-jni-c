67b5aaecf5265aac4c0b180ec6c9374966d86534
dhcarmona <dhcarmona@gmail.com>
2014-03-23 23:43:58 -0600

    Compilación Final
    Faltaban los .java por alguna razón.

--- CalculadoraJNI/src/calculadorajni/Aritmetica.java
+++ CalculadoraJNI/src/calculadorajni/Aritmetica.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package calculadorajni;
 
 public class Aritmetica{
     static {
-      System.loadLibrary("Aritmetica"); // Load native library at runtime
-                                   // hello.dll (Windows) or libhello.so (Unixes)
+      System.loadLibrary("Aritmetica"); 
    }
     public native double suma(double op1, double op2);
     public native double resta(double op1, double op2);
